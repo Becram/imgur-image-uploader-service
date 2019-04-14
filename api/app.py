4feb0f437f0905bf4b35ba6936894e875b06aae2
Becram <itsbecram@gmail.com>
2019-04-15 00:29:22 +0545

    first commit

---
+++ api/app.py
@@ -0,0 +1,26 @@
+from flask import Flask
+from flask import url_for
+from worker import celery
+import celery.states as states
+
+app = Flask(__name__)
+
+
+@app.route('/add/<int:param1>/<int:param2>')
+def add(param1: int, param2: int) -> str:
+    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
+    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+    return response
+
+
+@app.route('/check/<string:task_id>')
+def check_task(task_id: str) -> str:
+    res = celery.AsyncResult(task_id)
+    if res.state == states.PENDING:
+        return res.state
+    else:
+        return str(res.result)
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=8080, debug=True)
+
---
+++ api/docker_script.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+python /api/api/app.py runserver 
+#bash
