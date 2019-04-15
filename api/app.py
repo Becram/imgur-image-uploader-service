9c8719ce1d588248da667ad04ac8454b8f48d3e3
Bikram <bikram@bikram.olenepal>
2019-04-15 17:15:27 +0545

    working upload

--- api/app.py
+++ api/app.py
@@ -1,26 +1,75 @@
-from flask import Flask
+from flask import *
 from flask import url_for
 from worker import celery
 import celery.states as states
+from flask_debugtoolbar import DebugToolbarExtension
+import logging
+from datetime import datetime
 
 app = Flask(__name__)
+app.debug = True
+app.config['SECRET_KEY'] = 'Dont tell'
+toolbar = DebugToolbarExtension(app)
+
+UPLOAD_FOLDER = "/upload_files"
+pending=[]
+completed=[]
+failed=[]
 
 
-@app.route('/add/<int:param1>/<int:param2>')
-def add(param1: int, param2: int) -> str:
-    task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
-    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+Jobs='''
+{
+	"jobs":[
+	   {
+        "id": "id",
+        "created": "created",
+        "finished": "finished",
+        "status": "in-progress",
+         "uploaded": {
+                "pending": "pending",
+                "completed": "completed",
+                "failed": null
+
+         }
+	   }
+
+	]
+
+}
+'''
+ 
+
+
+@app.route('/add')
+def add() -> str:
+    task = celery.send_task('tasks.upload')
+    app.logger.info(type(task))
+    response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a> <body> toolbar </body>"
     return response
+
+# @app.route('/add/<int:param1>/<int:param2>')
+# def add(param1: int, param2: int) -> str:
+#     task = celery.send_task('tasks.add', args=[param1, param2], kwargs={})
+#     response = f"<a href='{url_for('check_task', task_id=task.id, external=True)}'>check status of {task.id} </a>"
+#     return response
 
 
 @app.route('/check/<string:task_id>')
 def check_task(task_id: str) -> str:
     res = celery.AsyncResult(task_id)
-    if res.state == states.PENDING:
-        return res.state
-    else:
-        return str(res.result)
+    my_json=json.loads(Jobs)
+    my_json['jobs'][0]['id']=task_id
+    my_json['jobs'][0]['created']=datetime.now()
+    return jsonify(my_json)
+
+    # if res.state == states.PENDING:
+
+
+
+    #   return res.state
+    # else:
+    #   return str(res.result)
 
 if __name__ == '__main__':
-    app.run(host="0.0.0.0", port=8080, debug=True)
+ 	app.run(host="0.0.0.0", port=8080)
 
--- celery-queue/tasks.py
+++ celery-queue/tasks.py
@@ -1,15 +1,79 @@
 import os
 import time
 from celery import Celery
+import logging
+from logging.handlers import RotatingFileHandler
+import requests
+import sys
 
 
-CELERY_BROKER_URL = os.environ.get('CELERY_BROKER_URL', 'redis://localhost:6379'),
-CELERY_RESULT_BACKEND = os.environ.get('CELERY_RESULT_BACKEND', 'redis://localhost:6379')
+CELERY_BROKER_URL = os.environ.get(
+	'CELERY_BROKER_URL', 'redis://localhost:6379'),
+CELERY_RESULT_BACKEND = os.environ.get(
+	'CELERY_RESULT_BACKEND', 'redis://localhost:6379')
 
-celery = Celery('tasks', broker=CELERY_BROKER_URL, backend=CELERY_RESULT_BACKEND)
+UPLOAD_FOLDER = "/upload_files"
+
+ALLOWED_EXTENSIONS = set(['json', 'jpeg', 'png', 'jpg'])
+Client_ID = 'e5451a6ff4ab502'
+
+celery = Celery('tasks', broker=CELERY_BROKER_URL,
+				backend=CELERY_RESULT_BACKEND)
+
+
+def get_filename(image_url):
+	if image_url.find('/'):
+		return image_url.rsplit('/', 1)[1]
+
+
+def download_image(image_url):
+	DEST = "/upload_files"
+	response = requests.get(image_url, stream=True)
+
+	# print(response.text)
+	# response.raise_for_status()
+	with open(DEST+"/"+get_filename(image_url), "wb") as imageFile:
+		for chunk in response.iter_content(1024):
+			imageFile.write(chunk)
+	return DEST+"/"+get_filename(image_url)
+
+
+
+def upload_imgur(image_name):
+
+	data = {}
+	imgur_upload_url = "https://api.imgur.com/3/image"
+	files = {}
+	payload = {'image': open(image_name, 'rb')}
+	headers = {
+		'Authorization': 'Client-ID {}'.format(Client_ID)
+	}
+	response = requests.request(
+		'POST', imgur_upload_url, headers=headers, files=payload)
+	try:
+		response_data=response.json()
+		data['id'] = response.json()['data']['id']
+		data['status'] = response.json()['status']
+		data['link'] = response.json()['data']['link']
+		json_data = data
+		# logger.info(str(json_data))
+		return str(json_data)
+	except KeyError:
+		link = "Failed"
+		return link
+
+
 
 
 @celery.task(name='tasks.add')
 def add(x: int, y: int) -> int:
-    time.sleep(5)
-    return x + y
+	time.sleep(5)
+	return x + y
+
+
+@celery.task(name='tasks.upload')
+def upload() -> str:
+    img_url= "https://cdn0.tnwcdn.com/wp-content/blogs.dir/1/files/2018/06/instagram-796x431.png"
+    image_link = upload_imgur(download_image(img_url))    
+    return image_link
+
