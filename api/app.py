758c9e7d92c4f2f2ec704d3c56758a279aa39b06
Bikram <bikram@bikram.olenepal>
2019-04-18 17:07:57 +0545

    working 1st version

--- api/app.py
+++ api/app.py
@@ -7,31 +7,33 @@ from flask_debugtoolbar import DebugToolbarExtension
 import logging
 from datetime import datetime
 from pprint import pprint
+from jobs import job_list
+import random
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Dont tell'
-
+file_handler = logging.FileHandler('app.log')
 
 UPLOAD_FOLDER = "/upload_files"
 pending = []
 completed = []
 
-urls = '''
-    {
-    "urls": [
-        "https://farm3.staticflickr.com/2879/11234651086_681b3c2c00_b_d.jpg",
-        "https://farm4.staticflickr.com/3790/11244125445_3c2f32cd83_k_d.jpg"
-        ]
-    }
+# urls = '''
+#     {
+#     "urls": [
+#         "https://farm3.staticflickr.com/2879/11234651086_681b3c2c00_b_d.jpg",
+#         "https://farm4.staticflickr.com/3790/11244125445_3c2f32cd83_k_d.jpg"
+#         ]
+#     }
 
-'''
+# '''
 
 
-Jobs = '''
+jobs = [
 {
-        "id": "id",
+        "id": 'id',
         "created": "created",
-        "finished": null,
+        "finished": None,
         "status": "in-progress",
          "uploaded": {
                 "pending": [],
@@ -40,50 +42,133 @@ Jobs = '''
 
         }
 }
-'''
-my_json = json.loads(Jobs)
-
-def get_urllist(data):
-    urls_json = json.loads(data)
-    url_list = []
-    for value in urls_json['urls']:
-        url_list.append(value)
-    return url_list
-
-
-@app.route('/add')
-def upload():
-
-    task=celery.send_task('tasks.upload', args=[urls])
-    response = f"<a href='{url_for('check_task', task_id=task.id)}'>check status of {task.id} </a> <body> toolbar </body>"
-    return response
+]
 
 
 
-@app.route('/check/<string:task_id>')
-def check_task(task_id: str) -> str:
-    res = celery.AsyncResult(task_id)
-    my_json['id'] = task_id
+@app.route('/v1/images/jobs', methods=['GET'])
+def get_jobs():
+    return jsonify({'jobs': [job for job in jobs]})
+
+
+
+
+
+
+@app.route('/v1/images/upload', methods=['POST'])
+def create_job():
+    if not request.json or 'urls' not in request.json:
+        abort(400)
+    pprint(request.json)
+    celery_job=celery.send_task('tasks.upload', args=[json.dumps(request.json)])
+    pprint(celery_job.id) 
+    job = {
+        'id': celery_job.id,
+        "created": datetime.now(),
+        "finished": None,
+        "status": None,
+        'uploaded':{
+            "pending": [],
+            "completed": [],
+            "failed": []
+        }
+    }
+
+    jobs.append(job)
+
+    # response = f" check status of  <a href='{url_for('check_task', task_id=task.id)}'>{task.id} </a> "
+    return jsonify({"jobId": celery_job.id})
+
+
+
+
+
+
+
+# @app.route("/api/funny")
+# def serve_funny_quote():
+#     upload_jobs = job_list()
+#     nr_of_jobs = len(upload_jobs)
+#     return jsonify(upload_jobs)
+
+
+
+
+# # my_job[0] = json.loads(Jobs)
+
+# def get_urllist(data):
+#     urls_json = json.loads(data)
+#     url_list = []
+#     for value in urls_json['urls']:
+#         url_list.append(value)
+#     return url_list
+
+
+# @app.route('/add')
+# def upload():
+
+#     task=celery.send_task('tasks.upload', args=[urls])
+#     response = f"<a href='{url_for('check_task', task_id=task.id)}'>check status of {task.id} </a> <body> toolbar </body>"
+#     return response
+
+@app.route('/v1/images/upload/<string:jobId>', methods=['GET'])
+def get_job(jobId):
+    res = celery.AsyncResult(jobId)
+    my_job = [job for job in jobs if job['id'] == jobId ]
+    if len(my_job) == 0:
+        abort(404)
+
     if res.state == 'in-progress':
         pprint("********PENING RESULT: {}".format(res.info))
         if res.info.get('created'):
-            my_json['created']=res.info.get('created')
+            my_job[0]['created']=res.info.get('created')
         if res.info.get('pending'):
-            my_json['uploaded']['pending']=res.info.get('pending')
-        if res.info.get('completed') and res.info.get('completed') not in my_json['uploaded']['completed']:
-            my_json['uploaded']['completed'].append(res.info.get('completed'))
-        return jsonify(my_json)
+            my_job[0]['uploaded']['pending']=res.info.get('pending')
+        if res.info.get('completed') and res.info.get('completed') not in my_job[0]['uploaded']['completed']:
+            my_job[0]['uploaded']['completed'].append(res.info.get('completed'))
+        return jsonify(my_job[0])
     else:
         pprint("********RESULT: {}".format(res.info))
-        if res.info.get('completed') and res.info.get('completed') not in my_json['uploaded']['completed']:
-            my_json['uploaded']['completed']=res.info.get('completed')
+        if res.info.get('completed') and res.info.get('completed') not in my_job[0]['uploaded']['completed']:
+            my_job[0]['uploaded']['completed']=res.info.get('completed')
         if res.info.get('created'):
-            my_json['created']=res.info.get('created')
-        my_json['uploaded']['pending']= None
-        my_json['finished']= res.info.get('finished')
-        my_json['status']= res.info.get('status')
-        return jsonify(my_json)
-    
+            my_job[0]['created']=res.info.get('created')
+        my_job[0]['uploaded']['pending']= None
+        my_job[0]['finished']= res.info.get('finished')
+        my_job[0]['status']= res.info.get('status')
+        return jsonify(my_job[0])
+    # return jsonify(my_job[0])
+
+
+
+
+
+
+# @app.route('/v1/images/upload/<string:jobId>')
+# def check_task(jobId: str) -> str:
+#     res = celery.AsyncResult(jobId)
+#     json_job = get_job(jobId)
+#     pprint("****{}".format(type(json_job)))
+#     # if res.state == 'in-progress':
+#     #     pprint("********PENING RESULT: {}".format(res.info))
+#     #     if res.info.get('created'):
+#     #         my_job[0]['created']=res.info.get('created')
+#     #     if res.info.get('pending'):
+#     #         my_job[0]['uploaded']['pending']=res.info.get('pending')
+#     #     if res.info.get('completed') and res.info.get('completed') not in my_job[0]['uploaded']['completed']:
+#     #         my_job[0]['uploaded']['completed'].append(res.info.get('completed'))
+#     #     return jsonify(my_job[0])
+#     # else:
+#     #     pprint("********RESULT: {}".format(res.info))
+#     #     if res.info.get('completed') and res.info.get('completed') not in my_job[0]['uploaded']['completed']:
+#     #         my_job[0]['uploaded']['completed']=res.info.get('completed')
+#     #     if res.info.get('created'):
+#     #         my_job[0]['created']=res.info.get('created')
+#     #     my_job[0]['uploaded']['pending']= None
+#     #     my_job[0]['finished']= res.info.get('finished')
+#     #     my_job[0]['status']= res.info.get('status')
+#     #     return jsonify(my_job[0])
+#     return json.jsonify(json_job)
 
     # if res.state == states.PENDING:
 
--- api/jobs.py
+++ api/jobs.py
@@ -1,8 +1,20 @@
-def jobs():
+def job_list():
     return[{
         "id": "id",
         "created": "created",
-        "finished": null,
+        "finished": None,
+        "status": "in-progress",
+         "uploaded": {
+                "pending": [],
+                "completed": [],
+                "failed": []
+
+        }
+    },
+    {
+        "id": "id",
+        "created": "created",
+        "finished": None,
         "status": "in-progress",
          "uploaded": {
                 "pending": [],
